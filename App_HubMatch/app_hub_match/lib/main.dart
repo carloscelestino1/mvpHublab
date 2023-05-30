30995689222846e0c0368836660849a47b732047
Arthur Henrique <arthurnam@outlook.com>
2023-05-30 10:39:21 -0300

    commit

---
+++ App_HubMatch/app_hub_match/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_hub_match/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ App_HubMatch/app_hub_match/lib/screens/login_screen.dart
@@ -0,0 +1,211 @@
+import 'package:app_hub_match/screens/forgot_passaword.dart.dart';
+import 'package:app_hub_match/screens/welcome_screen.dart';
+import 'package:app_hub_match/values/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _showPassoword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors().getWhiteColor(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: constraints.maxWidth * .9,
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const WelcomeScreen()));
+                        },
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.arrow_back_ios_new,
+                              size: 35,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/HubMatchLogo.png",
+                      height: 250,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: MaterialButton(
+                        color: CustomColors().getColorGreen(),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.g_mobiledata,
+                              color: CustomColors().getWhiteColor(),
+                              size: 50,
+                            ),
+                            const Text(
+                              "Entrar com o google",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              fillColor: Colors.white54,
+                              filled: true,
+                              border: OutlineInputBorder(),
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              prefixIcon: Icon(
+                                Icons.mail_outline,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              fillColor: Colors.white54,
+                              filled: true,
+                              border: const OutlineInputBorder(),
+                              labelText: "Senha",
+                              labelStyle: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              prefixIcon: const Icon(
+                                Icons.lock,
+                                color: Colors.black,
+                              ),
+                              suffixIcon: GestureDetector(
+                                child: Icon(
+                                  _showPassoword == false
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                  color: Colors.black,
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    _showPassoword = !_showPassoword;
+                                  });
+                                },
+                              ),
+                            ),
+                            obscureText: _showPassoword == false ? true : false,
+                          )
+                        ],
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(bottom: 10)),
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: CustomColors().getColorGreen(),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                        onPressed: () {},
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 20,
+                      ),
+                    ),
+                    GestureDetector(
+                      // ignore: avoid_print
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ForgotPassaword()));
+                      },
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Esqueceu a senha? ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Clique aqui',
+                              style: TextStyle(
+                                color: CustomColors().getColorGreen(),
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
